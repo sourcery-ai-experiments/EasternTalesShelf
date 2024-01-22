f711365881c4c920554ef4fb8e2614e565b766c4
Shiroe <maciejbielecki99@gmail.com>
2024-01-22 19:53:06 +0100

    fixed filter status and metia type

--- static/scripts.js
+++ static/scripts.js
@@ -7,34 +7,7 @@ function toggleMenu() {
     }
 }
 
-/*
-function applyFilters() {
-    var form = document.getElementById('filterForm');
-    var status = form.status.value;
-    var country = form.country_made.value;
-    var type = form.media_type.value;
 
-    var items = document.querySelectorAll('.grid-item');
-
-    items.forEach(function(item) {
-        var itemStatus = item.getAttribute('data-status');
-        var itemCountry = item.getAttribute('data-country');
-        var itemType = item.getAttribute('data-type');
-
-        if ((status === '' || itemStatus === status) &&
-            (country === '' || itemCountry === country) &&
-            (type === '' || itemType === type)) {
-            item.style.display = ''; // Show the item
-        } else {
-            item.style.display = 'none'; // Hide the item
-        }
-    });
-    
-    // Call the adjustAOS function to recheck if AOS needs to be enabled or disabled
-    adjustAOS();
-    AOS.refresh();
-}
-*/
 function filterEntries() {
     const titleFilter = document.getElementById('titleFilter').value.toLowerCase();
     const countryJapan = document.getElementById('countryJapan').checked;
